f1fb2b4c309f0d1ddb9c64d117dddf963de33cfd
Shubhangi Biradar <shubhangibiradar717@gmail.com>
2023-11-11 17:02:21 -0500

    added about app activity

---
+++ app/src/main/java/com/example/yelpmefind/AboutApp.kt
@@ -0,0 +1,12 @@
+package com.example.yelpmefind
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class AboutApp: AppCompatActivity() {
+
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.about_app)
+        }
+}
